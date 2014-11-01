5dd956816ec4f4d9583a8a1b978f94846da4850a
Ray Hua Wu <dzaefn@mit.edu>
2014-11-01 17:38:57 -0400

    finished producing of death chart

--- ebola.js
+++ ebola.js
@@ -3,15 +3,41 @@ var HALF_CELL_SIZE = Math.floor(CELL_SIZE/2);
 var CELL_MARGIN = 2;
 var CELL_ROWS = 81;
 var CELL_COLS = 81;
-var CANVAS_HORZ_SIZE = CELL_SIZE*CELL_ROWS+1;
-var CANVAS_VERT_SIZE = CELL_SIZE*CELL_COLS+1;
+var START_MARGIN = 8;
+var BOTTOM_MARGIN = 44;
+var CANVAS_HORZ_SIZE = START_MARGIN+CELL_SIZE*CELL_ROWS+1;
+var CANVAS_VERT_SIZE = START_MARGIN+CELL_SIZE*CELL_COLS+1+BOTTOM_MARGIN;
 var canvas = Raphael(0,0,CANVAS_HORZ_SIZE,CANVAS_VERT_SIZE);
 
-var COLORS = { 'Liberia':'#008080' , 'Sierra Leone':'#00C040' , 'Guinea':'#808000' , 'Uganda':'#808080' , 'Congo':'#C04000' , 'Gabon':'#000080' , 'Zaire':'#800080', 'Nigeria':'#008000' , 'Sudan':'#800000', 'Other':'#4000C0' };
+var COLORS = { 'Liberia':'#00C0C0' , 'Sierra Leone':'#00C040' , 'Guinea':'#A0A000' , 'Uganda':'#C00000' , 'Congo':'#F0A000' , 'Gabon':'#000080' , 'Zaire':'#C000C0', 'Nigeria':'#008000' , 'Sudan':'#8080F0', 'Other':'#808080' };
+
+var COLOR_DIM = { '#':'#' , 'F':'7', 'E':'7' , 'D':'6' , 'C':'6' , 'B':'5' , 'A':'5' , '9':'4' , '8':'4' , '7':'3' , '6':'3' , '5':'2' , '4':'2' , '3':'1' , '2':'1' , '1':'0' , '0':'0' };
+
+/* given hex for color, get color of similar hue but approximately half luminosity */
+function getDimmedColor(color){
+  var newColor = '';
+  for(var i=0;i<color.length;i++){
+    newColor += COLOR_DIM[color[i]];
+  }
+  return newColor;
+}
+
+/* produces string to represent a line from (x1,y1) to (x2,y2) */
+function parseLine(x1,y1,x2,y2){
+  var lineString = "m";
+  lineString += x1;
+  lineString += ",";
+  lineString += y1;
+  lineString += "l";
+  lineString += x2-x1;
+  lineString += ",";
+  lineString += y2-y1;
+  return lineString;
+}
 
 /* converts a number for the row or column of a cell (from 0 to CELL_ROWS or CELL_COLS) to a pixel quantity */
 function convertToScale(num){
-  return CELL_SIZE*num+HALF_CELL_SIZE;
+  return CELL_SIZE*num+HALF_CELL_SIZE+START_MARGIN;
 }
 
 /* draws a cell */
@@ -224,3 +250,22 @@ for(var c=22;c<81;c++){
 for(var c=64;c<81;c++){
   drawCell(c,r,COLORS['Liberia']);
 }
+
+var sudanText = canvas.text(convertToScale(2),convertToScale(81.5),'Sudan').attr({'font-family':'Ubuntu'}).attr({'fill':COLORS['Sudan']}).attr({'font-size':'15px'}).attr({'font-weight':'bold'});
+var drcText = canvas.text(convertToScale(11),convertToScale(81.5),'Zaire/Dem. Rep. of Congo').attr({'font-family':'Ubuntu'}).attr({'fill':COLORS['Zaire']}).attr({'font-size':'15px'}).attr({'font-weight':'bold'});
+var gabonText = canvas.text(convertToScale(20),convertToScale(81.5),'Gabon').attr({'font-family':'Ubuntu'}).attr({'fill':COLORS['Gabon']}).attr({'font-size':'15px'}).attr({'font-weight':'bold'});
+var ugandaText = canvas.text(convertToScale(25),convertToScale(81.5),'Uganda').attr({'font-family':'Ubuntu'}).attr({'fill':COLORS['Uganda']}).attr({'font-size':'15px'}).attr({'font-weight':'bold'});
+var repCongoText = canvas.text(convertToScale(32),convertToScale(81.5),'Rep. of Congo').attr({'font-family':'Ubuntu'}).attr({'fill':COLORS['Congo']}).attr({'font-size':'15px'}).attr({'font-weight':'bold'});
+var guineaText = canvas.text(convertToScale(39),convertToScale(81.5),'Guinea').attr({'font-family':'Ubuntu'}).attr({'fill':COLORS['Guinea']}).attr({'font-size':'15px'}).attr({'font-weight':'bold'});
+var sierraLeoneText = canvas.text(convertToScale(45),convertToScale(81.5),'Sierra Leone').attr({'font-family':'Ubuntu'}).attr({'fill':COLORS['Sierra Leone']}).attr({'font-size':'15px'}).attr({'font-weight':'bold'});
+var liberiaText = canvas.text(convertToScale(51),convertToScale(81.5),'Liberia').attr({'font-family':'Ubuntu'}).attr({'fill':COLORS['Liberia']}).attr({'font-size':'15px'}).attr({'font-weight':'bold'});
+var nigeriaText = canvas.text(convertToScale(56),convertToScale(81.5),'Nigeria').attr({'font-family':'Ubuntu'}).attr({'fill':COLORS['Nigeria']}).attr({'font-size':'15px'}).attr({'font-weight':'bold'});
+var otherText = canvas.text(convertToScale(61),convertToScale(81.5),'Other').attr({'font-family':'Ubuntu'}).attr({'fill':COLORS['Other']}).attr({'font-size':'15px'}).attr({'font-weight':'bold'});
+
+var divide1 = canvas.path(parseLine(convertToScale(18.5),START_MARGIN/2,convertToScale(18.5),convertToScale(65.5))).attr({'stroke-width':2}).attr({'stroke-dasharray':'-'});
+var divide2 = canvas.path(parseLine(convertToScale(18.5),convertToScale(65.5),convertToScale(21.5),convertToScale(65.5))).attr({'stroke-width':2}).attr({'stroke-dasharray':'-'});
+var divide3 = canvas.path(parseLine(convertToScale(21.5),convertToScale(65.5),convertToScale(21.5),convertToScale(79.5))).attr({'stroke-width':2}).attr({'stroke-dasharray':'-'});
+var divide4 = canvas.path(parseLine(convertToScale(21.5),convertToScale(79.5),convertToScale(63.5),convertToScale(79.5))).attr({'stroke-width':2}).attr({'stroke-dasharray':'-'});
+var divide5 = canvas.path(parseLine(convertToScale(63.5),convertToScale(79.5),convertToScale(63.5),CANVAS_VERT_SIZE-2*START_MARGIN)).attr({'stroke-width':2}).attr({'stroke-dasharray':'-'});
+
+var westAfricanEpidemicText = canvas.text(convertToScale(0.5),CANVAS_VERT_SIZE-START_MARGIN,"Deaths in the 21 previous outbreaks of ebola are to the left of the dashed line; deaths for the West African outbreak are to the right, a number that continues to grow.").attr({'font-family':'Ubuntu'}).attr({'text-anchor':'start'}).attr({'font-size':'12px'});
