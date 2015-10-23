241d329d3903e00d362a57196feef2d44c5901a8
Ray Hua Wu <dzaefn@mit.edu>
2015-10-23 17:07:58 -0400

    removed irrelevant file

--- ebolaComparison.js
+++
@@ -1,6 +0,0 @@
-var comparison = Raphael(0,0,800,800);
-
-var gunMurdersCircle = comparison.circle(600,250,194).attr({'stroke-width':0}).attr({'fill':'#800000'});
-var ebolaCircle = comparison.circle(300,500,162).attr({'stroke-width':0}).attr({'fill':'#00E000'});
-var gunMurdersText = comparison.text(600,250,'number of people who died in gun murders in the US in the gregorian year 2010').attr({'font-family':'Ubuntu'}).attr({'fill':'#FFFFFF'});
-var ebolaCircle = comparison.text(300,500,'number of people who died of ebola in the world, ever').attr({'font-family':'Ubuntu'});
